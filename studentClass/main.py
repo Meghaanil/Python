20e33224a437c288790c950f2169507ca766604e
Megha Anil <85555452+Meghaanil@users.noreply.github.com>
2023-06-28 20:47:35 +0530

    Add files via upload

---
+++ studentClass/main.py
@@ -0,0 +1,11 @@
+from studentClass import Student
+
+# create a new student instance
+student1 = Student("Megha", "India", 21)
+
+# display student details
+student1.display_student_details()
+
+# set and get age
+student1.set_age(18)
+print(student1.get_age())
